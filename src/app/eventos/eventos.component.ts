8e3e1981a3dee8019d84879ca694b563431e9779
arcdeoliveira <arcdeoliveira@gmail.com>
2022-07-22 19:13:35 -0300

    Construction of the nav of the project and the list page events in angular.

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -7,18 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public events: any
+
+  public events        : any = [];
+  public eventsFiltered: any = [];
+
+  public imgWith   : number  = 150;
+  public imgMargin : number  = 2;
+  public showImg   : boolean = true;
+  public textButton: string  = "";
+
+  private _filterList: string  = "";
+
+  public get filterList() {
+    return this._filterList;
+  }
+
+  public set filterList(value:string) {
+    this._filterList = value;
+    this.eventsFiltered = this.filterList ? this.filterEvents(this.filterList) : this.events;
+  }
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.textButton = EventosComponent.UpdateTextButton(this.showImg);
     this.GetEvents();
   }
 
   public GetEvents() : void {
     this.http.get("https://localhost:44346/api/event").subscribe(
-      response => this.events = response,
+      response => {
+        this.events         = response;
+        this.eventsFiltered = this.events;
+
+      },
       error    => console.error(error)
     );
+  }
+
+  filterEvents(filterList: string): any {
+    filterList = filterList.toLocaleLowerCase();
+
+    return this.events.filter(
+      (event : any) => event.theme.toLocaleLowerCase().indexOf(filterList) !== -1 ||
+                       event.local.toLocaleLowerCase().indexOf(filterList) !== -1
+    );
+  }
+
+  public HideOrShowImages(): void {
+    this.showImg    = !this.showImg;
+    this.textButton = EventosComponent.UpdateTextButton(this.showImg);
+  }
+
+  private static UpdateTextButton(showimage: boolean): string {
+    return showimage ? "Ocultar" : "Exibir";
   }
 }
